dc4c7c834e7feb5509ad9292072e08fbf3913a5b
bokutotu <mushin.hudoushin@gmail.com>
2025-01-27 03:51:11 +0900

    haskell

--- migrations/20240408143711_user_name_unique.sql
+++
@@ -1,2 +0,0 @@
--- Add migration script here
-ALTER TABLE users ADD COLUMN user_id TEXT UNIQUE NOT NULL DEFAULT gen_random_uuid();
--- src/db/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod group;
-pub mod user;
-pub mod user_group;
